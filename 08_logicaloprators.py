6347ba88519818278ccdb8d94f03faa6dab82f91
Abbadaisar <abbadaisar639589@gmail.com>
2026-07-09 12:20:26 +0530

    Chapter 2 assignment,

---
+++ 08_logicaloprators.py
@@ -0,0 +1,15 @@
+a = True
+b = False
+
+print(a and b)
+print(a or b)
+print(not a)
+print(not b)
+
+a = True
+b = False
+
+print("AND:", a and b)
+print("OR:", a or b)
+print("NOT a:", not a)
+print("NOT b:", not b)
